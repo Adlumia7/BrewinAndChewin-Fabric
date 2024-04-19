e678516b0d92df74701dbe643267e1ae678df1b6
mrsterner <simon@sterner.dev>
2024-04-19 12:56:46 +0200

    fixed raw meat tag

--- src/main/java/dev/sterner/brewinandchewin/common/registry/BCTags.java
+++ src/main/java/dev/sterner/brewinandchewin/common/registry/BCTags.java
@@ -6,9 +6,10 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import vectorwing.farmersdelight.FarmersDelight;
 
 public interface BCTags {
-    TagKey<Item> RAW_MEATS = TagKey.create(Registries.ITEM, new ResourceLocation(BrewinAndChewin.MODID, "raw_meats"));
+    TagKey<Item> RAW_MEATS = TagKey.create(Registries.ITEM, new ResourceLocation(FarmersDelight.MODID, "raw_meats"));
     TagKey<Item> HORROR_LASAGNA_MEATS = TagKey.create(Registries.ITEM, new ResourceLocation(BrewinAndChewin.MODID, "horror_lasagna_meats"));
     TagKey<Item> OFFHAND_EQUIPMENT = TagKey.create(Registries.ITEM, new ResourceLocation(BrewinAndChewin.MODID, "offhand_equipment"));
     TagKey<Block> FREEZE_SOURCES = TagKey.create(Registries.BLOCK, new ResourceLocation(BrewinAndChewin.MODID, "freeze_sources"));
